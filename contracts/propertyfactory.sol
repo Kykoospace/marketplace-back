04676821b19114d9d86fe57f6fb431ec3cb4b103
NealKemissi <neal.k@hotmail.fr>
2020-01-25 17:47:52 +0100

    new version

--- contracts/propertyfactory.sol
+++ contracts/propertyfactory.sol
@@ -1,32 +1,47 @@
-pragma solidity ^0.5.11;
+pragma solidity >=0.4.25 < 0.6.0;
+pragma experimental ABIEncoderV2;
 
-import "./ownable.sol";
-
-contract PropertyFactory is Ownable {
-
-    event newProperty(uint propertyId, string name, uint price, string addressProperty);
+contract PropertyFactory {
 
     struct Property {
+        uint idProperty;
         string name;
         uint price;
         string addressProperty;
+        address owner;
     }
 
     Property[] public properties;
 
-    mapping (uint => address) public propertyToOwner;
-    mapping (address => uint) ownerPropertyCount;
+    
+    address saleAddress;
+    address purshaseAddress;
 
-    // Création d'un bien
-    function createProperty(string memory _name, uint _price, string memory _addressProperty) public {
-        uint id = properties.push(Property(_name, _price, _addressProperty)) - 1;
-        propertyToOwner[id] = msg.sender;
-        ownerPropertyCount[msg.sender]++;
-        emit newProperty(id, _name, _price, _addressProperty);
+    event buyingProperty(address _saleAddress, address _purshaseAddress, uint price);
+
+    constructor() public {
+        purshaseAddress = msg.sender;
     }
 
-    modifier onlyOwnerOf(uint _propertyId) {
-    require(msg.sender == propertyToOwner[_propertyId]);
-    _;
-  }
+    // Création d'un bien
+    function createProperty(string memory _name, uint _price, string memory _addressProperty, address _owner) public {
+        uint id = properties.length + 1;
+        properties.push(Property(id, _name, _price, _addressProperty, _owner));
+    }
+
+    // Achat d'un bien
+    function purshaseProperty(uint _idProperty) external payable returns(bool) {
+        for(uint i = 0; i < properties.length; i++) {
+            if(properties[i].idProperty == _idProperty) {
+                require((msg.value - properties[i].price) > 0, "Argent insuffisant, transaction impossible");
+                // ACHAT
+                saleAddress = properties[i].owner;
+                saleAddress.transfer(msg.value);
+                emit buyingProperty(saleAddress, purshaseAddress, msg.value);
+                properties[i].owner = msg.sender;
+                return true;
+            }
+        }
+        return false;
+    }
 }
--- contracts/propertyhelper.sol
+++ contracts/propertyhelper.sol
@@ -5,29 +5,53 @@ import "./propertyfactory.sol";
 
 contract PropertyHelper is PropertyFactory {
 
-function changeName(uint _propertyId, string calldata _newName) external onlyOwnerOf(_propertyId) {
+// tous les biens d'un owner
+function getPropertiesByOwner(address _owner) public view returns(Property[] memory) {
+  uint memory size = getTotalOfPropertiesByOwner(_owner);
+  Property[] memory result = new Property[](size);
+  uint counter = 0;
+  for(uint i = 0 ; i < properties.length; i++) {
+    if(properties[i].owner == _owner) {
+      result[counter] = properties[i];
+      counter++;
+    }
+  }
+  return result;
+}
+
+// le nb de biens d'un owner
+function getTotalOfPropertiesByOwner(address _owner) public view returns(uint memory) {
+  uint cpt = 0;
+  for(uint i = 0 ; i < properties.length; i++) {
+    if(properties[i].owner == _owner) {
+      cpt++;
+    }
+  }
+  return cpt;
+}
+
+// tous les biens
+function getAllProperties() public view returns(Property[] memory) {
+    return properties;
+}
+
+// un bien en particulier
+function getProperty(uint _idProperty) public view returns(Property memory) {
+  for(uint i = 0 ; i < properties.length; i++) {
+    if(properties[i].idProperty == _idProperty) {
+      return properties[i];
+    }
+  }
+}
+
+// changement de nom d'un bien
+function changeName(uint _propertyId, string calldata _newName) external {
   properties[_propertyId].name = _newName;
 }
 
-function changeAddressProperty(uint _propertyId, string calldata _newAddressProperty) external onlyOwnerOf(_propertyId) {
+// changement d'adresse d'un bien
+function changeAddressProperty(uint _propertyId, string calldata _newAddressProperty) external {
   properties[_propertyId].addressProperty = _newAddressProperty;
 }
 
-function getPropertiesByOwner(address _owner) public view returns(uint[] memory) {
-    uint[] memory result = new uint[](ownerPropertyCount[_owner]);
-    uint counter = 0;
-    for (uint i = 0; i < properties.length; i++) {
-      if (propertyToOwner[i] == _owner) {
-        result[counter] = i;
-        counter++;
-      }
-    }
-    return result;
-  }
-
-  function getAllProperties() public view returns(Property[] memory) {
-      return properties;
-  }
 }
-
-

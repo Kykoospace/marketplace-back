35603b45fa4a11b70309bc6ecb3def85840dc20d
Kyllian <kyllian.gt@hotmail.fr>
2020-01-26 18:05:15 +0100

    Correctifs avant soutenance

--- contracts/propertyfactory.sol
+++ contracts/propertyfactory.sol
@@ -44,4 +44,53 @@ contract PropertyFactory {
         }
         return false;
     }
+
+    // tous les biens d'un owner
+    function getPropertiesByOwner(address _owner) public view returns(Property[] memory) {
+      uint size = getTotalOfPropertiesByOwner(_owner);
+      Property[] memory result = new Property[](size);
+      uint counter = 0;
+      for(uint i = 0 ; i < properties.length; i++) {
+        if(properties[i].owner == _owner) {
+          result[counter] = properties[i];
+          counter++;
+        }
+      }
+      return result;
+    }
+
+    // le nb de biens d'un owner
+    function getTotalOfPropertiesByOwner(address _owner) public view returns(uint) {
+      uint cpt = 0;
+      for(uint i = 0 ; i < properties.length; i++) {
+        if(properties[i].owner == _owner) {
+          cpt++;
+        }
+      }
+      return cpt;
+    }
+
+    // tous les biens
+    function getAllProperties() public view returns(Property[] memory) {
+        return properties;
+    }
+
+    // un bien en particulier
+    function getProperty(uint _idProperty) public view returns(Property memory) {
+      for(uint i = 0 ; i < properties.length; i++) {
+        if(properties[i].idProperty == _idProperty) {
+          return properties[i];
+        }
+      }
+    }
+
+    // changement de nom d'un bien
+    function changeName(uint _propertyId, string memory _newName) public {
+      properties[_propertyId].name = _newName;
+    }
+
+    // changement d'adresse d'un bien
+    function changeAddressProperty(uint _propertyId, string memory _newAddressProperty) public {
+      properties[_propertyId].addressProperty = _newAddressProperty;
+    }
 }
